93b265896384138a70a957e26eb5732aea51ce03
AyushSingh172000 <ayushsingh17102000@gmail.com>
2025-02-18 11:30:01 +0530

    decoration-box

--- Flutter-Project/demo_project/lib/main.dart
+++ Flutter-Project/demo_project/lib/main.dart
@@ -28,7 +28,35 @@ class DashBoardScreen extends StatelessWidget {
       ),
 
       body: Container(
-        color: Colors.redAccent,
+        //width: double.infinity, //Takes full width
+        //height: double.infinity, //Takes full screen height
+        color: Colors.red.shade500,
+        child: Center(
+          child: Container(
+            width: 150,
+            height: 150,
+            // color: Colors.blueGrey, it will throw an erro because when we use decoration box we have to declare the color inside that box.
+            decoration: BoxDecoration(
+              color: Colors.blueGrey,
+              //borderRadius: BorderRadius.circular(20)
+              //borderRadius: BorderRadius.only(topLeft: Radius.circular(20), bottomRight: Radius.circular(20))
+              //borderRadius: BorderRadius.all(Radius.circular(21)),
+              border: Border.all(
+                width: 2,
+                color: Colors.white
+              ),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 11,
+                  color: Colors.white,
+                  spreadRadius: 7
+                )
+              ],
+              shape: BoxShape.circle, // when we applying this we have to remove the border radius because it will give the error. In circle there will be no need of border radius.
+            ),
+          ),
+        ),
+
       ),
     );
     
